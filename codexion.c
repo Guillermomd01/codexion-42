1ead5e7b2663d16a02066e811e75ef84e6e5e35d
ubuntuguille <md.guillermo1@gmail.com>
2026-03-03 18:39:01 +0100

    main function

---
+++ codexion.c
@@ -0,0 +1,32 @@
+
+
+
+
+
+#include "codexion.h"
+
+int	main(int argc, char **argv)
+{
+	t_data		data;
+	pthread_t	monitor;
+	int			i;
+
+	if (parsing(&data, argc, argv) != 0)
+	{
+		printf("Use: ./codexion n_coders t_burnout t_compile t_debug t_refactor n_compiles t_cooldown scheduler\n");
+		return (1);
+	}
+	if (init_codexion(&data) != 0)
+		return (1);
+	data.start = get_time_in_ms();
+	i = -1;
+	while (++i < data.n_coders)
+		pthread_create(&data.coder[i].thread, NULL, (void *)coder_routine, &data.coder[i]);
+	pthread_create(&monitor, NULL, monitor_routine, &data);
+	i = -1;
+	while (++i < data.n_coders)
+		pthread_join(data.coder[i].thread, NULL);
+	pthread_join(monitor, NULL);
+	destroy_data(&data, 3);
+	return (0);
+}
